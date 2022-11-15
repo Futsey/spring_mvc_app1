08e855fa484d2d894b41ee36bf2aacff5fcbcc87
Fut <shmopls@gmail.com>
2022-11-15 12:05:20 +0500

    Модель + Simple WebCalculator

--- src/main/java/ru/futsey/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/futsey/springcourse/config/controllers/FirstController.java
@@ -1,46 +1,46 @@
 package ru.futsey.springcourse.config.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
-    /**
-     * В методе GET ожидаем имя пользователя (синтаксис .../hello?name=Somename) посредством использования
-     * тяжелого класса, способного получить всю информацию о запросе
-     * @see HttpServletRequest
-     * @param request
-     * @return view
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-    String name = request.getParameter("name");
-        System.out.println("Hello " + name);
-        return "first/hello";
-    }
-     */
-
-    /**
-     * В методе GET ожидаем только имя пользователя посредством аннотации @RequestParam
-     * Дополнительно явно определяем параметр required
-     * во избежание ошибки 400 (в данном случае по умолчанию поле name == null)
-     * @param name
-     * @return view
-     */
-    @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name) {
-        System.out.println("Hello " + name);
+    public String helloPage(@RequestParam(value="name", required = false) String name, Model model) {
+        model.addAttribute("message", "Hello, " + name);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodBye";
+    }
 
+    @GetMapping("/calculator")
+    public String helloPage(@RequestParam(value="a", required = false) int a,
+                            @RequestParam(value="b", required = false) int b,
+                            @RequestParam(value="action", required = false) String action, Model model) {
+        double rsl = 0D;
+        switch (action) {
+            case "multiplication":
+                rsl = a * b;
+                break;
+            case "addition":
+                rsl = a + b;
+                break;
+            case "substruction":
+                rsl = a - b;
+                break;
+            case "division":
+                rsl = a / (double) b;
+                break;
+        }
+        model.addAttribute("result", rsl);
+        return "first/calculator";
     }
 }
